1c84b7a8a1f532e94bc1f4e473c809f54193e571
Anna-Voronina <127735848+Anna-Voronina@users.noreply.github.com>
2023-07-16 13:23:48 +0000

    Deploying to gh-pages from @ Anna-Voronina/goit-react-hw-08-phonebook@6bb1827ed0d5bcc1378cacbbfdb6add7b4bd2891 🚀

---
+++ static/js/692.12997ea0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[692],{7692:function(t,a,e){e.d(a,{yNL:function(){return r}});var c=e(9983);function r(t){return(0,c.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M19 2H5a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h4l3 3 3-3h4a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zm-7 3c1.727 0 3 1.272 3 3s-1.273 3-3 3c-1.726 0-3-1.272-3-3s1.274-3 3-3zM7.177 16c.558-1.723 2.496-3 4.823-3s4.266 1.277 4.823 3H7.177z"}}]})(t)}}}]);
+//# sourceMappingURL=692.12997ea0.chunk.js.map
